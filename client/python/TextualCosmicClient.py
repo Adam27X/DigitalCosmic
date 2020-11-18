be6415211cc4e64ea6ab6f1b304bbaa5e1f123e3
Adam McLaughlin <adam27x@gmail.com>
2020-11-17 23:38:22 -0500

    Add initial GUI for Python client

--- client/python/TextualCosmicClient.py
+++ client/python/TextualCosmicClient.py
@@ -2,39 +2,69 @@
 
 import socket
 import sys
+from Tkinter import *
+import ttk
 
-#In our case the server sends and receives strings directly, so there's no need to use repr() or other methods to change the sent/received data to a string format
-def read_message_from_server(s0):
-    buf = s0.recv(4096) #The initial server messages are actually sometimes larger than 1 kB
-    return buf
+class CosmicClient:
+    def __init__(self,root):
+        root.title("Textual Cosmic")
 
-def send_message_to_server(s0,msg):
-    s0.sendall(msg)
+        mainframe = ttk.Frame(root, padding="3 3 12 12")
+        mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+        root.columnconfigure(0, weight=1)
+        root.rowconfigure(0, weight=1)
 
-server_ip = raw_input('Enter the IP address of the host server:\n')
-server_port = raw_input('Enter the port of the host server:\n')
+        self.server_ip = StringVar()
+        server_ip_entry = ttk.Entry(mainframe, textvariable=self.server_ip)
+        server_ip_entry.grid(column=1,row=0)
 
-print 'Connecting to server at ' + server_ip + ':' + server_port
+        self.server_port = StringVar()
+        server_port_entry = ttk.Entry(mainframe, textvariable=self.server_port)
+        server_port_entry.grid(column=1,row=1)
 
-#Create a socket
-s0 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s0.connect((server_ip,int(server_port)))
+        ttk.Button(mainframe, text="Connect", command=self.connect_to_server).grid(column=0, row=2)
 
-while True:
-    buf = read_message_from_server(s0)
-    needs_response = False
-    if buf == 'END':
-        break
-    elif buf.find('[needs_response]') != -1:
-        needs_response = True
+        ttk.Label(mainframe, text='Server IP address:').grid(column=0,row=0)
+        ttk.Label(mainframe, text='Server Port:').grid(column=0, row=1)
 
-    if needs_response:
-        print buf
-        response = raw_input('How would you like to respond?\n')
-        send_message_to_server(s0,response)
-    else:
-        print buf
+        root.bind("<Return>", self.connect_to_server)
 
-s0.close()
+        self.s0 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-sys.exit(0)
+    def connect_to_server(self,*args):
+        self.s0.connect((self.server_ip.get(),int(self.server_port.get())))
+        root.destroy()
+
+        #TODO: Create a new window for the actual game state...we probably don't want to use the root window for making connections either
+        #      We can probably use the root window for game state and just hide it until we've connected
+        #TODO: Use asyncio to execute this loop
+        while True:
+            buf = self.read_message_from_server()
+            needs_response = False
+            if buf == 'END':
+                break
+            elif buf.find('[needs_response]') != -1:
+                needs_response = True
+
+            if needs_response:
+                print buf
+                response = raw_input('How would you like to respond?\n')
+                self.send_message_to_server(response)
+            else:
+                print buf
+
+        sys.exit(0)
+
+    #In our case the server sends and receives strings directly, so there's no need to use repr() or other methods to change the sent/received data to a string format
+    def read_message_from_server(self):
+        buf = self.s0.recv(4096) #The initial server messages are actually sometimes larger than 1 kB
+        return buf
+
+    def send_message_to_server(self,msg):
+        self.s0.sendall(msg)
+
+
+root = Tk()
+CosmicClient(root)
+root.mainloop()
+
